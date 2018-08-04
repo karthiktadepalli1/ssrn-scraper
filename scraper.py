7e5a028eca56322512cb3005419a98649bbbb105
Karthik <karthik.tadepalli1@gmail.com>
2018-08-04 13:54:59 -0400

    updated error handling

--- scraper.py
+++ scraper.py
@@ -21,31 +21,32 @@ linkList = []
 for x in range(10000):
     linkList.append("https://papers.ssrn.com/sol3/papers.cfm?abstract_id=" + str(x))
 
-paperList = []
-
 
 def quickSoup(url):
-    header = {}
-    header['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
-    page = requests.get(url, headers=header)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    return(soup)
+    try:
+        header = {}
+        header['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+        page = requests.get(url, headers=header, timeout=10)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        return(soup)
+    except Exception:
+        return(None)
 
 
 def getPaper(url):
     try:
         error = "The abstract you requested was not found"
         article = quickSoup(url)
-        if error not in article.get_text() and article.find('h1') is not None:
-            title = article.find('h1').get_text()
-            paperList.append(url + "," + title)
-            return(url + "," + title)
+        if article is not None:
+            if error not in article.get_text() and article.find('h1') is not None:
+                title = article.find('h1').get_text()
+                return(url + "," + title)
     except Exception as e:
         return(None)
 
 
 def scrape():
-    numWorkers = cpu_count() * 8
+    numWorkers = cpu_count() * 4
     print(numWorkers)
     p = Pool(numWorkers)
 

ea7f639f890a686a8b35577dd6edd3b6bbea93c0
Karthik <karthik.tadepalli1@gmail.com>
2020-09-14 21:07:23 +0530

    fixed functionality and consolidated scripts

--- scraper.py
+++ scraper.py
@@ -1,34 +1,17 @@
 import requests
+from ordered_set import OrderedSet
 from bs4 import BeautifulSoup
 from multiprocessing import Pool
 from multiprocessing import cpu_count
-
-
-"""
-def compilePapers():
-    error = "The abstract you requested was not found"
-    for i in range(1000, 2000):
-        url = "https://papers.ssrn.com/sol3/papers.cfm?abstract_id=" + str(i)
-        article = quickSoup(url)
-
-        if error not in article.get_text() and article.find('h1') is not None:
-            title = article.find('h1').get_text()
-            paperList.append(url + "," + title)
-
-"""
-
-linkList = []
-for x in range(100000):
-    linkList.append("https://papers.ssrn.com/sol3/papers.cfm?abstract_id=" + str(x))
-counter = 0
-
+import time
+import sys
 
 def quickSoup(url):
     try:
         header = {}
         header['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
-        page = requests.get(url, headers=header, timeout=10)
-        soup = BeautifulSoup(page.content, 'html.parser')
+        # page = requests.get(url, headers=header, timeout=10)
+        soup = BeautifulSoup(requests.get(url, headers=header, timeout=10).content, 'html.parser')
         return(soup)
     except Exception:
         return(None)
@@ -36,41 +19,81 @@ def quickSoup(url):
 
 def getPaper(url):
     try:
-        error = "The abstract you requested was not found"
         article = quickSoup(url)
-        if article is not None:
-            if error not in article.get_text() and article.find('h1') is not None:
-                title = article.find('h1').get_text()
-                title = title.replace(",", "")
-                return(url + "," + title)
-        print(url)
-    except Exception as e:
-        return(None)
+        t = article.get_text()
+        if "The abstract you requested was not found" in t:
+            return("{},".format(url))
+        title = article.find('h1').get_text().replace("\n", "")
+        test_list = OrderedSet(t.split("\n"))
+        authors = test_list[0].replace(title, "").replace(" :: SSRN", "").replace(" by ", "").replace(", ", ":")
+        date = [line.replace("Last revised: ", "") for line in test_list if "Last revised: " in line]
+        if date == []:
+            date = [line.replace("Posted: ", "") for line in test_list if "Last revised: " in line]
+        date = date[0]
+        text = t.split("Abstract\n")[1]
+        abstract = "\"{}\"".format(text.split("Suggested Citation:")[0].replace("\n", ""))
+
+        # get paper statistics
+        stats = OrderedSet(article.find('div', attrs = {'class': 'box-paper-statics'}).get_text().split("\n"))
+        views, dl, rank, refs = "", "", "", ""
+        try:
+            views = stats[stats.index('Abstract Views') + 1].strip().replace(",", "")
+        except:
+            pass
+        try:
+            dl = stats[stats.index('Downloads') + 1].strip().replace(",", "")
+        except:
+            pass
+        try:
+            rank = stats[stats.index('rank') + 1].strip().replace(",", "")
+        except:
+            pass
+        try:
+            refs = stats[stats.index('References') + 1].strip().replace(",", "")
+        except:
+            pass
+        results = [url, "\"{}\"".format(title), abstract, authors, date, views, dl, rank, refs]
+        return(",".join(results))
+    except:
+        return("{},,,,,,,,".format(url))
 
 
-def scrape():
-    numWorkers = cpu_count() * 4
+def dummyscrape(start, stop):
+    numWorkers = cpu_count() * 12
     print(numWorkers)
     p = Pool(numWorkers)
+    linkList = ["https://papers.ssrn.com/sol3/papers.cfm?abstract_id=" + str(x) for x in range(start, stop)]
 
     papers = p.map(getPaper, linkList)
     p.terminate()
     p.join()
-    writePapers = []
-    for paper in papers:
-        if paper is not None:
-            writePapers.append(paper)
-    writeString = "\n".join(writePapers)
-    with open('ssrn-links.csv', 'w+') as f:
+    writeString = "\n".join(papers)
+    with open('test.csv', 'w+') as f:
         f.write(writeString)
 
-    print("Whew! Done with that!")
+def scrape(start, stop):
+    numWorkers = cpu_count() * 12
+    p = Pool(numWorkers)
+    linkList = ["https://papers.ssrn.com/sol3/papers.cfm?abstract_id={}".format(str(x)) for x in range(start, stop)]
 
+    papers = p.map(getPaper, linkList)
+    p.terminate()
+    p.join()
+    writeString = "\n".join(papers)
+    with open('ssrn-links.csv', 'a') as f:
+        f.write(writeString)
 
 if __name__ == "__main__":
-    linkList = []
-    for x in range(100000):
-        linkList.append("https://papers.ssrn.com/sol3/papers.cfm?abstract_id=" + str(x))
-    scrape()
-
-# for 3500: 560 seconds with 8 workers, 287 with 32, 309 with 40, 325 with 64
+    # import cProfile
+    # import scraper
+    # globals().update(vars(scraper))
+    # sys.modules['__main__'] = scraper
+    # dummyscrape(4000, 4100)
+    breaks = [10000 * x for x in range(179, 250)]
+    t = time.time()
+    for i in range(len(breaks)-1):
+        print(breaks[i])
+        b = time.time()
+        scrape(breaks[i], breaks[i+1])
+        print("TIME FOR 1000: " + str(time.time() - b))
+        print("TIME SINCE START: " + str(time.time() - t))
